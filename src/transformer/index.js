da162ac9892444170ecafa58e6064b5cbb842c2e
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-01-31 08:10:46 +0000

    Transform document

--- src/transformer/index.js
+++ src/transformer/index.js
@@ -1,5 +1,14 @@
-import transformFromSchema from 'shinkansen-transmission/lib/transmission/from-schema'
-import transformIntoValues from 'shinkansen-transmission/lib/transmission/into-values'
+import fromSchema from 'shinkansen-transmission/lib/transmission/from-schema'
+import fromDocument from 'shinkansen-transmission/lib/transmission/from-document'
+
+export const hasEnum = (field = {}) => Reflect.has(field, 'enum')
+export const getEnum = (field = {}) => Reflect.get(field, 'enum')
+
+export const hasOneOf = (field = {}) => Reflect.has(field, 'oneOf')
+export const getOneOf = (field = {}) => Reflect.get(field, 'oneOf')
+
+export const hasAnyOf = (field = {}) => Reflect.has(field, 'anyOf')
+export const getAnyOf = (field = {}) => Reflect.get(field, 'anyOf')
 
 export const getType = ({ type } = {}) => (type ? { type } : {})
 
@@ -13,21 +22,58 @@ export const transformTitleToLabel = (field = {}, text) => ({ ...field, ...(text
 
 export const transformTitleToLegend = (field = {}, text) => ({ ...field, ...(text ? { legend: { text } } : {}) })
 
+export const transformEnum = ({ items = [], selectedIndex, name, id = name, ...field }) => ({
+  ...field,
+  items: items.map((text, value) => ({ text, value, selected: value === selectedIndex })),
+  ...(name ? { name } : {}),
+  ...(id ? { id } : {})
+})
+
 export const transformField = ({ name, id = name, ...field } = {}) => ({ ...field, ...(name ? { name } : {}), ...(id ? { id } : {}) })
+
+export const transformElementsForEnum = ({
+  title,
+  description,
+  enum: {
+    ...field
+  }
+} = {}) => transformDescriptionToHint(transformTitleToLabel(transformEnum(field), title), description)
+
+export const transformElementsForField = ({
+  title,
+  description,
+  field
+} = {}) => transformDescriptionToHint(transformTitleToLabel(transformField(field), title), description)
 
 export const transformFields = (fields = []) => fields.map(transform)
 
+export const transformElements = (elements) => {
+  if (hasEnum(elements)) {
+    return {
+      enum: transformElementsForEnum(elements)
+    }
+  } else {
+    if (hasOneOf(elements)) {
+
+    } else {
+      if (hasAnyOf(elements)) {
+
+      } else {
+        return {
+          params: transformElementsForField(elements)
+        }
+      }
+    }
+  }
+}
+
 export const transformOne = ({
   meta = {},
-  elements: {
-    title,
-    description,
-    field
-  } = {}
+  elements = {}
 } = {}) => ({
   ...getType(meta),
   ...getComponent(meta),
-  params: transformDescriptionToHint(transformTitleToLabel(transformField(field), title), description)
+  ...transformElements(elements)
 })
 
 export const transformAll = ({
@@ -57,4 +103,4 @@ const transform = (schema = {}) => {
     : transformOne(schema)
 }
 
-export default (definition, response, components) => transform(transformFromSchema(definition, transformIntoValues(response), components))
+export default (definition, response, components) => transform(fromSchema(definition, fromDocument(response, definition), components))
