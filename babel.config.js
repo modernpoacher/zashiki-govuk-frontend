dd2175fbe8dcff4c00d163e1cda00dc921d9ecee
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-24 14:33:04 +0100

    Babel

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,56 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        targets: {
-          node: 'current'
-        },
-        useBuiltIns: 'entry',
-        corejs: 3
-      }
-    ]
-  ],
-  plugins: [
-    '@babel/proposal-export-default-from',
-    [
-      'module-resolver', {
-        alias: {
-          'zashiki-govuk-frontend': '.'
-        }
-      }
-    ]
+const debug = require('debug')
+
+const log = debug('zashiki:transformer')
+
+const {
+  env: {
+    DEBUG = 'zashiki:transformer',
+    NODE_ENV = 'development'
+  }
+} = process
+
+debug.enable(DEBUG)
+
+log('`zashiki` is awake')
+
+function env () {
+  log({ NODE_ENV })
+
+  return (
+    NODE_ENV === 'production'
+  )
+}
+
+const presets = [
+  [
+    '@babel/env', {
+      targets: {
+        node: '12.18.1'
+      },
+      useBuiltIns: 'entry',
+      corejs: 3
+    }
   ]
+]
+
+const plugins = [
+  '@babel/proposal-export-default-from',
+  [
+    'module-resolver', {
+      alias: {
+        'zashiki-govuk-frontend': '.'
+      }
+    }
+  ]
+]
+
+module.exports = (api) => {
+  if (api) api.cache.using(env)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
